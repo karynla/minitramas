503092088a6fd1140dd6830568241472b8226c93
Karyn <99372279+karynla@users.noreply.github.com>
2024-10-07 11:24:20 -0300

    commit aquivos base

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+      <h1>Te amo mo</h1>
+
+      <p>Resposta:</p>
+      <img src="/images/desafio.dev.png" alt="resposta" />
+    </div>
+  );
+}
+
+export default Home;
